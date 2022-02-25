eb14aa654f34cfd57d78e8dc7cd78aac24cc2c33
george lucas <glalmeida.dev@gmail.com>
2022-02-25 09:58:58 -0300

    Requisito 1

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,2 +1,31 @@
-test('', () => {});
-const test = 'vamo com tudo';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import App from '../App';
+
+describe('Testando Requisito 2', () => {
+  beforeEach(() => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+  });
+
+  it('Teste se a página contém as informações sobre a Pokédex.', () => {});
+
+  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    // const InfoPokedex = screen.getAllByRole('p');
+    // expect(InfoPokedex).toHaveLength(2);
+  });
+  it('Testando se a página contém um heading h2, com o texto About Pokédex', () => {
+    // const heading = screen.getByRole('heading', { level: 2 });
+    // expect(heading).toHaveTextContent(/About Pokédex/i);
+  });
+
+  it('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+    // const image = getByRole('img');
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,56 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+describe('Testando Requisito 1', () => {
+  beforeEach(() => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+  });
+
+  it('O primeiro link deve possuir o texto Home', () => {
+    const link = screen.getByRole('link', { name: 'Home' });
+    expect(link).toBeInTheDocument();
+  });
+
+  it('O segundo link deve possuir o texto About', () => {
+    const link2 = screen.getByRole('link', { name: 'About' });
+    expect(link2).toBeInTheDocument();
+  });
+
+  it('O segundo link deve possuir o texto Favorite Pokémons', () => {
+    const link3 = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(link3).toBeInTheDocument();
+  });
+
+  it('teste se a página é redirecionada para About - Click_About', () => {
+    const link21 = screen.getByRole('link', { name: 'About' });
+    userEvent.click(link21);
+    const tittle21 = screen.getByRole('heading', { level: 2, name: /About Pokédex/i });
+    expect(tittle21).toBeInTheDocument();
+  });
+
+  it('teste se a página é redirecionada para pokeF - Click_Pokémons Favoritados', () => {
+    const link31 = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(link31);
+    const tittle31 = screen
+      .getByRole('heading', { level: 2, name: /Favorite pokémons/i });
+    expect(tittle31).toBeInTheDocument();
+  });
+
+  it('teste se a página é redirecionada para a página inicial - Click_Home', () => {
+    const link = screen
+      .getByRole('link', { name: 'Home' });
+    userEvent.click(link);
+    const tittle = screen
+      .getByRole('heading', { level: 2, name: /Encountered pokémons/i });
+    expect(tittle).toBeInTheDocument();
+  });
+});
