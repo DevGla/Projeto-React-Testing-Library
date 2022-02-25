5641ccf045865bf5bea3a8b6eb391f427a83c4d0
george lucas <glalmeida.dev@gmail.com>
2022-02-25 11:28:52 -0300

    Requisito 1 - certo

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
 import { Router } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
 describe('Testando Requisito 2', () => {
@@ -14,15 +15,28 @@ describe('Testando Requisito 2', () => {
     );
   });
 
-  it('Teste se a página contém as informações sobre a Pokédex.', () => {});
+  it('Teste se a página contém as informações sobre a Pokédex.', () => {
+    const link = screen.getByRole('link', { name: /about/i });
+    userEvent.click(link);
+    const text = screen.getByText(/This application simulates a Pokédex/i);
+    expect(text).toBeInTheDocument();
+    const text2 = screen.getByText(/One can filter Pokémons by type/i);
+    expect(text2).toBeInTheDocument();
+  });
 
   it('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
-    // const InfoPokedex = screen.getAllByRole('p');
-    // expect(InfoPokedex).toHaveLength(2);
+    const link2 = screen.getByRole('link', { name: /about/i });
+    userEvent.click(link2);
+    const text1 = screen.getByText(/This application simulates a Pokédex/i);
+    expect(text1).toBeInTheDocument();
+    const text21 = screen.getByText(/One can filter Pokémons by type/i);
+    expect(text21).toBeInTheDocument();
   });
   it('Testando se a página contém um heading h2, com o texto About Pokédex', () => {
-    // const heading = screen.getByRole('heading', { level: 2 });
-    // expect(heading).toHaveTextContent(/About Pokédex/i);
+    const link = screen.getByRole('link', { name: /about/i });
+    userEvent.click(link);
+    const heading = screen.getByRole('heading', { level: 2 });
+    expect(heading).toHaveTextContent(/About Pokédex/i);
   });
 
   it('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
